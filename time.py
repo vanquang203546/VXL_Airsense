4591286665fab3fce28408cc5bd14f51a667424a
Van Quang <quanga4lh@gmail.com>
2023-07-16 23:14:52 +0700

    show date and time details from timestamp on time.py

--- time.py
+++ time.py
@@ -5,6 +5,13 @@ def subtract_days(timestamp, days):
     dt -= datetime.timedelta(days=days)
     new_timestamp = dt.timestamp()
     return new_timestamp
-timestamp = 1686755697
-new_timestamp = subtract_days(timestamp, 3)
-print(new_timestamp)
+def show_details(timestamp):
+    dt = datetime.datetime.fromtimestamp(timestamp)
+    date = dt.strftime("%d/%m/%Y")
+    time = dt.strftime("%H:%M:%S")
+    print("Ngày: ", date)
+    print("Giờ: ", time)
+def main():
+    timestamp = 1686755697
+    show_details(timestamp)
+main()
